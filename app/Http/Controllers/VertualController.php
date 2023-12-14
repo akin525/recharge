e8562b97427759427cc01f0d38a6385658b7f3a0
PIPER <akinlabi@gmail.com>
2023-12-14 17:10:41 +0100

    hello

--- app/Http/Controllers/VertualController.php
+++ app/Http/Controllers/VertualController.php
@@ -42,7 +42,7 @@ class VertualController
                 CURLOPT_POSTFIELDS => array('account_name' => $name,
                     'business_short_name' => 'Recharge','uniqueid' => $username,
                     'email' => $email,'dob' => "1991-05-27",
-                    'address' => "Ikeja Lagos Nigeria",'gender' => "Male",
+                    'address' => "Ikeja Lagos Nigeria",'gender' => "Male", 'provider'=>"providus",
                     'phone' =>'08146328645','webhook_url' => 'https://rechargestation.com.ng/api/run'),
                 CURLOPT_HTTPHEADER => array(
                     'Authorization: mcd_key_75rq4][oyfu545eyuriup1q2yue4poxe3jfd'
