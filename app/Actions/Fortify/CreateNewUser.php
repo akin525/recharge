5db4ee3dc12ba98cc3d935b94606394ff90f6fa6
PIPER <akinlabi@gmail.com>
2023-12-16 08:47:54 +0100

    hello

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -35,6 +35,52 @@ class CreateNewUser implements CreatesNewUsers
 
         return DB::transaction(function () use ($input) {
 
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => 'https://integration.mcd.5starcompany.com.ng/api/reseller/virtual-account3',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_SSL_VERIFYHOST => 0,
+                CURLOPT_SSL_VERIFYPEER => 0,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => array('account_name' => $input['name'],
+                    'business_short_name' => 'Recharge','uniqueid' => $input['username'],
+                    'email' => $input['email'],'dob' => "1991-05-27",
+                    'address' => "Ikeja Lagos Nigeria",'gender' => "Male", 'provider'=>"providus",
+                    'phone' =>'08146328645','webhook_url' => 'https://rechargestation.com.ng/api/run'),
+                CURLOPT_HTTPHEADER => array(
+                    'Authorization: mcd_key_75rq4][oyfu545eyuriup1q2yue4poxe3jfd'
+                ),
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+            $data = json_decode($response, true);
+            if ($data['success']==1){
+                $account = $data["data"]["customer_name"];
+                $number = $data["data"]["account_number"];
+                $bank = $data["data"]["bank_name"];
+
+//                $user->account_number = $number;
+//                $user->account_name = $account;
+//                $user->bank = $bank;
+//                $user->save();
+//
+//                Alert::success('Succeaa', 'Virtual Account Successful Created');
+//                return redirect("dashboard")->with('success', 'You are not allowed to access');
+
+
+            }elseif ($data['success']==0){
+                $account = "1";
+                $number = "1";
+                $bank = null;
+            }
             $receiver=$input ['email'];
             $admin= 'info@rechargestation.com.ng';
 //            Mail::to($receiver)->send(new Emailotp($input));
@@ -42,6 +88,9 @@ class CreateNewUser implements CreatesNewUsers
             return tap(User::create([
                 'name' => $input['name'],
                 'username'=>$input['username'],
+                'account_number'=$number,
+                'account_name'=$account,
+                'bank'=$bank,
                 'phone'=>$input['number'],
                 'email' => $input['email'],
                 'password' => Hash::make($input['password']),
